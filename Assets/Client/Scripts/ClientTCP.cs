39e25eabddb2f16e6184927c462bdd73e8fe65e5
Unknown <joshua.tobias@games-academy.de>
2018-10-25 16:24:51 +0200

    init

---
+++ Assets/Client/Scripts/ClientTCP.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Sockets;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Assets.Client.Scripts
+{
+    public class ClientTCP : MonoBehaviour
+    {
+        public static ClientTCP instance;
+
+        public TcpClient client;
+        public NetworkStream myStream;
+        private byte[] asyncBuffer;
+        public bool isConnected;
+
+        public byte[] receivedBytes;
+        public bool handleData = false;
+
+        private string IP_ADDRESS = "127.0.0.1";
+        private int PORT = 2711;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void Update()
+        {
+            if (handleData)
+            {
+                ClientHandlePackets.HandleData(receivedBytes);
+                handleData = false;
+            }
+        }
+
+        public void Connect()
+        {
+            Debug.Log("Trying to connect to the server");
+            client=new TcpClient();
+            client.ReceiveBufferSize = 4096;
+            client.SendBufferSize = 4096;
+            asyncBuffer= new byte[8192];
+            try
+            {
+                client.BeginConnect(IP_ADDRESS, PORT, new AsyncCallback(ConnectCallback), client);
+            }
+            catch (Exception)
+            {
+                Debug.Log("Unable to connect to the server");
+                throw;
+            }
+        }
+
+        private void ConnectCallback(IAsyncResult result)
+        {
+            try
+            {
+                client.EndConnect(result);
+                if (client.Connected == false)
+                {
+                    return;
+                }
+                else
+                {
+                    myStream = client.GetStream();
+                    myStream.BeginRead(asyncBuffer, 0, 8192, OnReceiveData, null);
+                    isConnected = true;
+                    Debug.Log("You are connected to the server succesfully");
+                }
+            }
+            catch (Exception)
+            {
+                isConnected = false;
+                throw;
+            }
+        }
+
+        private void OnReceiveData(IAsyncResult result)
+        {
+            try
+            {
+                int packetLength = myStream.EndRead(result);
+                receivedBytes = new byte[packetLength];
+                Buffer.BlockCopy(asyncBuffer,0,receivedBytes,0,packetLength);
+
+                if (packetLength == 0)
+                {
+                    Debug.Log("Disconnected");
+                    Application.Quit();
+                    return;
+                }
+
+                handleData = true;
+                myStream.BeginRead(asyncBuffer, 0, 8192, OnReceiveData, null);
+
+
+            }
+            catch (Exception)
+            {
+                Debug.Log("Disconnected");
+                Application.Quit();
+                return;
+            }
+        }
+
+        public void SendData(byte[] data)
+        {
+            ByteBuffer buffer = new ByteBuffer();
+            buffer.WriteLong((data.GetUpperBound(0) - data.GetLowerBound(0)) + 1);
+            buffer.WriteBytes(data);
+            myStream.Write(buffer.ToArray(),0,buffer.ToArray().Length);
+        }
+
+        public void SEND_THANKYOU()
+        {
+            ByteBuffer buffer = new ByteBuffer();
+            buffer.WriteLong((long)ClientPackets.C_THANKYOU);
+            buffer.WriteString("Thank you, server");
+            SendData(buffer.ToArray());
+        }
+    }
+}
---
+++ UnityServer/UnityServer/ServerHandlePackets.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityServer
+{
+    class ServerHandlePackets
+    {
+        private delegate void Packet_(long connectionID, byte[] data);
+
+        private static Dictionary<long, Packet_> packets;
+        private static long pLength;
+
+        public static void InitializePackets()
+        {
+            packets = new Dictionary<long, Packet_>();
+            packets.Add((long)ClientPackets.C_THANKYOU, PACKET_THANKYOU);
+        }
+
+        public static void HandleData(long connectionID, byte[] data)
+        {
+            byte[] Buffer;
+            Buffer = (byte[])data.Clone();
+
+            if (TCPServer.Client[connectionID].Buffer == null)
+            {
+                TCPServer.Client[connectionID].Buffer = new ByteBuffer();
+            }
+            TCPServer.Client[connectionID].Buffer.WriteBytes(Buffer);
+
+            if (TCPServer.Client[connectionID].Buffer.Count() == 0)
+            {
+                TCPServer.Client[connectionID].Buffer.Clear();
+                return;
+            }
+
+            if (TCPServer.Client[connectionID].Buffer.Length() >= 4)
+            {
+                pLength = TCPServer.Client[connectionID].Buffer.ReadLong(false);
+
+                if (pLength <= 0)
+                {
+                    TCPServer.Client[connectionID].Buffer.Clear();
+                    return;
+                }
+            }
+
+            while (pLength > 0 & pLength <= TCPServer.Client[connectionID].Buffer.Length() - 8)
+            {
+                if (pLength <= TCPServer.Client[connectionID].Buffer.Length() - 8)
+                {
+                    TCPServer.Client[connectionID].Buffer.ReadLong();
+                    data = TCPServer.Client[connectionID].Buffer.ReadBytes((int)pLength);
+                    HandleDataPackets(connectionID, data);
+                }
+
+                pLength = 0;
+                if (TCPServer.Client[connectionID].Buffer.Length() >= 4)
+                {
+                    pLength = TCPServer.Client[connectionID].Buffer.ReadLong(false);
+
+                    if (pLength <= 0)
+                    {
+                        TCPServer.Client[connectionID].Buffer.Clear();
+                        return;
+                    }
+                }
+
+                if (pLength <= 1)
+                {
+                    TCPServer.Client[connectionID].Buffer.Clear();
+                }
+            }
+        }
+
+        private static void HandleDataPackets(long connectionID, byte[] data)
+        {
+            long packetIdentifier;
+            ByteBuffer buffer;
+            Packet_ packet;
+
+            buffer = new ByteBuffer();
+            buffer.WriteBytes(data);
+            packetIdentifier = buffer.ReadLong();
+            buffer.Dispose();
+
+            if (packets.TryGetValue(packetIdentifier, out packet))
+            {
+                packet.Invoke(connectionID,data);
+            }
+        }
+
+        private static void PACKET_THANKYOU(long connectionID, byte[]data)
+        {
+            ByteBuffer buffer = new ByteBuffer();
+            buffer.WriteBytes(data);
+            long packetIdentifier = buffer.ReadLong();
+            string msg = buffer.ReadString();
+
+            Console.WriteLine(msg);
+        }
+    }
+}
